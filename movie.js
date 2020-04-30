00734d66ff4d2ff3dfd288d954aa28405ebdb44b
Istiyaq123 <57604080+Istiyaq123@users.noreply.github.com>
2020-05-01 00:39:51 +0100

    Create movie.js

---
+++ movie.js
@@ -0,0 +1,3 @@
+var m22 = document.getElementById("m22");
+
+m22.src = "https://www181.o0-1.com/token=npGFgowP1I3xxHYcFMrKBw/1588304303/86.133.0.0/115/e/cc/6971e56171c6b5d3232eef26afc31cce-480p.mp4";
