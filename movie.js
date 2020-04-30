879d7045dbd54ac80d73b1e626e4407d1499872f
Istiyaq123 <57604080+Istiyaq123@users.noreply.github.com>
2020-05-01 00:53:46 +0100

    Update movie.js

--- movie.js
+++ movie.js
@@ -1,3 +1,5 @@
 var m22 = document.getElementById("m22");
+var m5 = document.getElementById("m22");
 
 m22.src = "https://www181.o0-1.com/token=npGFgowP1I3xxHYcFMrKBw/1588304303/86.133.0.0/115/e/cc/6971e56171c6b5d3232eef26afc31cce-480p.mp4";
+m5.src = "https://www1986.o0-4.com/token=cBirltD27xA7GOnENJGfrQ/1588305088/86.133.0.0/44/6/ab/8b98f42bd261b7756f14d4355d195ab6-480p.mp4";
